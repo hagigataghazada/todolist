df1832f096c1fd9c0904af0489a0b9c593289124
Hagigat Aghazada <agazade.heqiqet@gmail.com>
2024-03-21 19:59:41 +0000

    todolist

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 "use strict";
-$(document).ready(function () {
 
+$(document).ready(function() {
+    $('.btn').click(function(e) {
+        e.preventDefault();
+        var inputValue = $('input').val().trim();
+        if (inputValue !== '') {
+            if (!isNaN(inputValue)) {
+                var num = parseInt(inputValue);
+                for (var i = 0; i < num; i++) {
+                    $('.box').append(`<h2>${num}</h2>`);
+                }
+            } else {
+                $('.box').append(`<h2>${inputValue}</h2>`);
+            }
+        }
+        $('input').val('');
+    });
 });
